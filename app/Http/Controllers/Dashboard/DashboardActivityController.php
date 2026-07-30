f09b0e0e12443cd41c9c57e69185d215375f3d87
shuraiken <realninjadude@gmail.com>
2026-07-31 00:17:05 +0800

    feat(dashboard): add dashboard activity controller

---
+++ app/Http/Controllers/Dashboard/DashboardActivityController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardActivityController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        
+    }
+}
