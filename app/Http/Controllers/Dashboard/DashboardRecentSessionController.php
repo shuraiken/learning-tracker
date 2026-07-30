75097c251137ef15acf6dffbc03565db6a59a82e
shuraiken <realninjadude@gmail.com>
2026-07-31 00:17:41 +0800

    feat(dashboard): add dashboard recent session controller

---
+++ app/Http/Controllers/Dashboard/DashboardRecentSessionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardRecentSessionController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        
+    }
+}
